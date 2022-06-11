451c299d951fbdd884eb650b91ad1948a96b97f7
MrRaghavan <raghavofbappa@gmail.com>
2022-06-12 00:17:51 +0530

    Greatest of two numbers

--- ProblemsOnNumbers/src/GreatestOfTwoNumbers.java
+++ ProblemsOnNumbers/src/GreatestOfTwoNumbers.java
@@ -8,13 +8,15 @@ public class GreatestOfTwoNumbers {
 	public static void main(String[] args) {
 		int number1 = 6;
 		int number2 = 13;
-		// System.out.println("Greatest of the two numbers " + number1 + " and " +
-		// number2 + " is " + Math.max(number1, number2));
+		System.out.println("Greatest of the two numbers " + number1 + " and " + number2 + " is " + Math.max(number1, number2));
 
-		if (number1 > number2) {
-			System.out.println(number1 + " is greater than " + number2);
+		double number3 = 1.123;
+		double number4 = 1.134;
+		
+		if (number3 > number4) {
+			System.out.println(number3 + " is greater than " + number4);
 		} else {
-			System.out.println(number2 + " is greater than " + number1);
+			System.out.println(number4 + " is greater than " + number3);
 		}
 	}
 
