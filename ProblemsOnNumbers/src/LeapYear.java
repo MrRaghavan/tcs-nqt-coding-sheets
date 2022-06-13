2d899814edfb8b609ea109fc704f1a9b3d96d43d
MrRaghavan <raghavofbappa@gmail.com>
2022-06-13 23:36:50 +0530

    Check if given year is a leap year or not

--- ProblemsOnNumbers/src/LeapYear.java
+++ ProblemsOnNumbers/src/LeapYear.java
@@ -14,7 +14,7 @@ public class LeapYear {
 	}
 
 	private static void checkIfLeapYear(int year) {
-		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+		if (((year % 4 == 0) || (year % 400 == 0) && (year % 100 != 0))) {
 			System.out.println(year + " is a leap year");
 		} else {
 			System.out.println(year + " is not a leap year");
